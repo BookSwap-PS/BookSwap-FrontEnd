f4038ada1a97b5a1f7cf95d650df68ecb71af9f4
LuizCPorto <luiz.porto@mail.uft.edu.br>
2024-09-26 22:41:10 -0300

    Refatoração tela home

--- BookSwap/src/pages/home/Home.tsx
+++ BookSwap/src/pages/home/Home.tsx
@@ -53,6 +53,12 @@ export default function ListLivro() {
         fetchLivros();
     }, []);
 
+    React.useEffect(() => {
+        navigation.setOptions({
+            headerShown: false, 
+        });
+    }, [navigation]);
+
     const onRefresh = async () => {
         setRefreshing(true);
         await fetchLivros();
